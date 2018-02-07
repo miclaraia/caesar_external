5a3abeecf6153ffef1bb603de66f5dc48991854d
Michael Laraia <micheal.laraia@gmail.com>
2018-02-07 14:24:28 -0600

    Using generator to load classifications
    Instead of loading everything upfront into memory

--- caesar_external/extractor.py
+++ caesar_external/extractor.py
@@ -10,19 +10,6 @@ import panoptes_client as pan
 
 
 class Extractor:
-    _instance = None
-
-    def __init__(self):
-        self.data = []
-
-    def queue(self, data):
-        self.data = data
-
-    @classmethod
-    def instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-        return cls._instance
 
     @classmethod
     def last_id(cls, next_id=None):
@@ -33,7 +20,6 @@ class Extractor:
     @classmethod
     def get_classifications(cls, last_id):
         project = Config.instance().project
-        data = []
         for c in Client.extract(project, last_id):
             cl = {
                 'id': int(c.id),
@@ -52,19 +38,12 @@ class Extractor:
                 session = c.raw['metadata']['session'][:10]
                 cl.update({'user': 'not-logged-in-%s' % session})
 
-            data.append(cl)
-
-        return data
+            yield cl
 
     @classmethod
     def next(cls):
-        data = cls.get_classifications(cls.last_id())
-
-        # for item in data:
-            # yield item
-        if len(data)>1:
-            cls.last_id(data[-1]['id'])
-
-        cls.instance().queue(data)
-        return data
-
+        cl = None
+        for cl in cls.get_classifications(cls.last_id()):
+            yield cl
+        if cl:
+            cls.last_id(cl['id'])
